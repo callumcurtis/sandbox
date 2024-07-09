75ece4dfd201feb7776ca30c0c4798b5fbb254b4
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-07-09 19:57:30 -0400

    chore: add shell for introduction to git machete

---
+++ experiments/introduction-to-git-machete/README.md
@@ -0,0 +1,10 @@
+# Introduction to Git Machete
+
+[git-machete](https://github.com/VirtusLab/git-machete) helps manage stacked PRs.
+The point of this project is to find out whether this tool would be helpful in
+my workflow.
+
+Why I'm interested:
+- Adds to the git interface (doesn't modify or replace it)
+- I'm curious if it helps maintaining multiple branches for multiple stacked PRs
+
---
+++ experiments/introduction-to-git-machete/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Introduction to Git Machete";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            git-machete
+          ];
+        };
+      });
+}
