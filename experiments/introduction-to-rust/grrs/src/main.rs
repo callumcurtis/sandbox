c5ce1dfc6e238f962c2fa11e205e87dcf614b367
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-10-06 21:35:42 -0400

    feat: add clap for argument parsing

--- experiments/introduction-to-rust/grrs/src/main.rs
+++ experiments/introduction-to-rust/grrs/src/main.rs
@@ -1,8 +1,15 @@
-use std::env::args;
+use clap::Parser;
+
+// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    // The pattern to search for
+    pattern: String,
+    // The path to the file to read
+    path: std::path::PathBuf,
+}
 
 fn main() {
-    let pattern = args().nth(1).expect("no pattern given");
-    let path = args().nth(2).expect("no path given");
-
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+    let args = Cli::parse();
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
