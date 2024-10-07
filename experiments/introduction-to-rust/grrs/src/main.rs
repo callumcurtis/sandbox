692aea6b642c1df53fec59c5a71c9ba11354f944
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2024-10-06 22:00:23 -0400

    feat: add read result unwrapping

--- experiments/introduction-to-rust/grrs/src/main.rs
+++ experiments/introduction-to-rust/grrs/src/main.rs
@@ -12,11 +12,10 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let result = std::fs::read_to_string(&args.path);
+    let content = match result {
+        Ok(c) => { c },
+        Err(_e) => { panic!("failed to read {}", &args.path.display()); }
+    };
+    println!("content={}", content);
 }
