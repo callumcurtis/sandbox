04f8a13a07670e3c67bfd763dc31cf4e9ef9f8a3
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-10-24 14:10:23 -0700

    chore: create image compressor experiment directory

---
+++ experiments/image-compressor/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Image compressor";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python313
+            python313Packages.numpy
+          ];
+        };
+      });
+}
