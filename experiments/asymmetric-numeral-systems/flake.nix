03ffd0484672a80db635b2a2aa4dae304754d4f0
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-12-04 10:37:06 -0800

    feat: add rans example

---
+++ experiments/asymmetric-numeral-systems/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Asymmetric Numeral Systems";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            gcc
+          ];
+        };
+      });
+}
---
+++ experiments/asymmetric-numeral-systems/rans/README.md
@@ -0,0 +1,6 @@
+## Resources
+
+- https://ww2.ii.uj.edu.pl/~smieja/teaching/ti/3a.pdf
+- https://en.wikipedia.org/wiki/Asymmetric_numeral_systems
+- https://github.com/rygorous/ryg_rans/tree/master (rANS)
+
